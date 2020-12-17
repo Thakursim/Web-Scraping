9949f542a7043a599e3ea64602018732a9586524
simran <simran2010thakur@gmail.com>
2020-12-17 10:45:35 +0530

    Optimised code

--- Being--Human.py
+++ Being--Human.py
@@ -5,48 +5,23 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
            'AppleWebKit/537.36 (KHTML, like Gecko) '\
            'Chrome/75.0.3770.80 Safari/537.36'}
            
-new_arrivals_url = 'https://www.beinghumanclothing.com/new-arrivals-being-human&sort=sort_order&order=desc&page={}&is_ajax=1'   
-for x in range(1, 8):
-    scrape_url = new_arrivals_url.format(x)
-    result = requests.get(scrape_url, headers=headers)
-    json_text = result.json()
-    product = json_text[0]
-    Bsoup = bs4.BeautifulSoup(product, "html")
-    product_name = Bsoup.find_all('h4', class_="product-name")
-    for y in product_name:
-        New_arrivals = "New-Arrivals", "->", y.text.strip()
-        f = open("Being--Human.txt", "a+")
-        n_str = " ".join(New_arrivals)
-        if len(New_arrivals) > 0:
-            f.write("\n")
-        f.write(n_str)
-        f.close()
-men_url = "https://www.beinghumanclothing.com/being-human-men&sort=sort_order&order=desc&page={}&is_ajax=1"
-for n in range(1, 26):
-    base_url = men_url.format(n)
-    res = requests.get(base_url, headers=headers)
-    men_text = res.json()
-    men_product = men_text[0]
-    BSoup = bs4.BeautifulSoup(men_product, 'html')
-    product_name_men = BSoup.find_all('h4', class_="product-name")
-    for m in product_name_men:
-        Men = "Men", "->", m.text.strip()
-        f = open("Being--Human.txt", "a+")
-        m_str = " ".join(Men)
-        if len(Men) > 0:
-            f.write("\n")
-        f.write(m_str)
-        f.close()
-mask_url = "https://www.beinghumanclothing.com/being-human-face-masks?plist=4"
-Result = requests.get(mask_url, headers=headers)
-soup = bs4.BeautifulSoup(Result.text, "html")
-mask_product_name = soup.find_all('h4', class_="product-name")
-for a in mask_product_name:
-    Mask = "Mask", "->", a.text.strip()
-    f = open("Being--Human.txt", "a+")
-    mask_str = " ".join(Mask)
-    if len(Mask) > 0:
-        f.write("\n")
-    f.write(mask_str)
-    f.close()
+b = [{"url": "https://www.beinghumanclothing.com/new-arrivals-being-human&sort=sort_order&order=desc&page=%s&is_ajax=1", "page_count": 8, "desc": "New-arrivals"}, {"url": "https://www.beinghumanclothing.com/being-human-men&sort=sort_order&order=desc&page=%s&is_ajax=1", "page_count": 26,
+"desc": "Men"}, {"url" : "https://www.beinghumanclothing.com/being-human-face-masks&page=%s&is_ajax=1", "page_count": 2, "desc": "Mask"}]	
+	
+for x in b:
+    for no in range(1, x['page_count']):
+        base_url = x['url'] % (no)
+        res = requests.get(base_url, headers=headers)
+        json_text = res.json()
+        product = json_text[0]
+        soup = bs4.BeautifulSoup(product, 'html')
+        product_name = soup.find_all('h4', class_='product-name')
+        for n in product_name:
+            list_of_products = x['desc'],"->", n.text.strip()
+            f = open("Being--Human.txt", "a+")
+            str_list = " ".join(list_of_products)
+            if len(list_of_products) > 0:
+               f.write("\n")
+            f.write(str_list)
+            f.close()
             
